34bda43aa6e3689b63e2cccd0965152b3e2975c9
Psychopomp3012 <114600784+Psychopomp3012@users.noreply.github.com>
2024-08-18 23:42:34 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -25,20 +25,14 @@ app.use(express.json());
 // app.use(cors());
 
 // CORS configuration
-const allowedOrigins = ['https://library-production-8c6a.up.railway.app'];
-app.use(cors({
-  origin: function (origin, callback) {
-    // allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type'],
-}));
+
+app.use(
+    cors({
+        origin: "*",
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type']
+    })
+);
 
 app.use('/books', booksRoute);
 
